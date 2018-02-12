b993af12d751308ec09a2d0dbba1cfada3a99775
Davit Mayilyan <davit.mayilyan@protonmail.ch>
2018-02-12 01:03:08 +0100

    Some clean-up implemented

--- wiki_parser.py
+++ wiki_parser.py
@@ -6,8 +6,26 @@ import os
 import wikipedia as wiki
 
 
+def in_range_arm(s):
+    other_chars = (' ', '(', ')')
+    if 1328 < ord(s) < 1423 or s in other_chars:
+        return 0
+    else:
+        return 1
+
 # Do the ckeaning. Need to decide on cleaning patter
-# def cleanup():
+def cleanup(p_content):
+    for lin in p_content:
+        if '=' in lin:
+            continue
+        if len(lin) == 0:
+            continue
+        print(lin)
+        print('/////////////////////////')
+        lin = ''.join([s for s in lin if not in_range_arm(s)])
+        #  Use double spaces to reset the pattern detection
+        print(lin)
+        print('????')
 
 
 def wiki_parse():
@@ -17,22 +35,26 @@ def wiki_parse():
     print(page.title)
     p_content = page.content
     print(p_content)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    p_content = page.content.splitlines()
 
-    f, path = create_temp()
-    with open(path, 'w') as temp_f:
-        temp_f.write(p_content)
+    cleanup(p_content)
 
-    print('--------------------------')
-    with open(path, 'r+') as temp_f:
-    # for line in f:
-        print(temp_f.read())
+    # f, path = create_temp()
+    # with open(path, 'w') as temp_f:
+    #     temp_f.write(p_content)
+
+    # print('--------------------------')
+    # print(path)
+    # with open(path, 'r+') as temp_f:
+    # # for line in f:
+    #     print(temp_f.read())
 
 
 # Creating a temp file
 def create_temp(prefix='tmp'):
     f, path = tempfile.mkstemp(prefix)
-    tempfile.mkstemp
-    tempfile.mkstemp
+    # tempfile.mkstemp
     remove_at_exit(f, path)
     return f, path
 
